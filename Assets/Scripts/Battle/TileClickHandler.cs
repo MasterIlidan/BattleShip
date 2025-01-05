d98b0aa6bafa395e6534d01b9f144ec730bd7df1
MasterIlidan <segamil2001@gmail.com>
2025-01-05 06:31:18 +0500

    Перемещение + релазация

---
+++ Assets/Scripts/Battle/TileClickHandler.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using DefaultNamespace;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TileClickHandler : MonoBehaviour
+{
+    public UnityEvent<string> OnTileClickedEvent;
+    private void OnEnable()
+    {
+        GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>().OnStateChanged.AddListener(Subscribe);
+        /*GameObject[] tiles = GameObject.FindGameObjectsWithTag("Tiles");
+        foreach (var tile in tiles)
+        {
+            tile.GetComponent<TileScript>().onTileClick.AddListener(OnTileClicked);
+        }*/
+    }
+
+    public void Subscribe(GameState state)
+    {
+        if (state != GameState.BATTLE) return;
+        GameObject[] tiles = GameObject.FindGameObjectsWithTag("Tiles");
+        foreach (var tile in tiles)
+        {
+            tile.GetComponent<TileScript>().onTileClick.AddListener(OnTileClicked);
+        }
+    }
+
+    private void OnDisable()
+    {
+        GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>().OnStateChanged.RemoveListener(Subscribe);
+        GameObject[] tiles = GameObject.FindGameObjectsWithTag("Tiles");
+        foreach (var tile in tiles)
+        {
+            tile.GetComponent<TileScript>().onTileClick.RemoveListener(OnTileClicked);
+        }
+    }
+
+    public void OnTileClicked(string name)
+    {
+        print("tile clicked " + name);
+        OnTileClickedEvent.Invoke(name);
+    }
+}
